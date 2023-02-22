3e9c89f1b11a6214b48dc533808e9f04603e2d8e
Galen Sho Sato <galen.sato@gmail.com>
2023-02-22 15:45:15 -0500

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    
+    def match(self, list):
+        arr = [letter for letter in self.word]
+        new_arr = []
+        for word in list:
+            if sorted(arr) == sorted([ltr for ltr in word]):
+                new_arr.append(word)
+        return new_arr
